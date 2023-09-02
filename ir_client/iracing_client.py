d2d5b7007677219165b11d2093d5e8c7885f919d
Sascha Lamp <sascha@lamp-online.de>
2023-09-02 23:02:05 +0200

    LEAGUE-7 added raw data method to the client

--- ir_client/iracing_client.py
+++ ir_client/iracing_client.py
@@ -10,28 +10,35 @@ class IracingClient:
     def __init__(self, protocol='https', host='members.iracing.com'):
         self.base_url = build_absolute_url(protocol, host)  # TODO get from config
 
+    def get_raw_data(self, endpoint_type_cls, endpoint_parameters, session_parameters):
+        endpoint_type = endpoint_type_cls()
+        results = self._send_request(endpoint_type, endpoint_parameters, session_parameters)
+        return endpoint_type.extract_data(results)
+
     def get_data(self, endpoint_type_cls, endpoint_parameters, session_parameters):
         endpoint_type = endpoint_type_cls()
-        headers = {
-            'Content-Type': 'application/x-www-form-urlencoded',  # TODO extract value
-            'Cookie': f'irsso_membersv2={session_parameters.cookie_irsso_membersv2}',  # TODO use proper cookie
-        }
-        url = endpoint_type.url(self.base_url)
-        results: requests.Response = requests.post(
-            url=str(url), 
-            headers=headers, 
-            data=endpoint_parameters.as_dict(),
-            allow_redirects=False
-        )
-
-        if self._has_authorization_failed(results):
-            raise AuthorizationFailedException()
-
+        results = self._send_request(endpoint_type, endpoint_parameters, session_parameters)
         try:
             return endpoint_type.map_data(results)
         except (JSONDecodeError, MappingException) as e:
             raise InvalidDataException(endpoint_type_cls, endpoint_parameters, results.text[:1000]) from e
     
+    def _send_request(self, endpoint_type, endpoint_parameters, session_parameters) -> requests.Response:
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',  # TODO extract value
+            'Cookie': f'irsso_membersv2={session_parameters.cookie_irsso_membersv2}',  # TODO use proper cookie
+        }
+        url = endpoint_type.url(self.base_url)
+        results = requests.post(
+            url=str(url), 
+            headers=headers, 
+            data=endpoint_parameters.as_dict(),
+            allow_redirects=False
+        )
+        if self._has_authorization_failed(results):
+            raise AuthorizationFailedException()
+        return results
+
     def _has_authorization_failed(self, response: requests.Response):
         return response.status_code == 302 and (
             "login.jsp" in response.headers['Location'] or "notauthed.jsp" in response.headers['Location']
